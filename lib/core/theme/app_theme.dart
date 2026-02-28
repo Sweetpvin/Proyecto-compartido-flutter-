96f99e2c8be385e2d3c7bb06a7886e939148d9ed
KingSadid <SadiSex07@gmail.com>
2026-02-28 18:10:47 -0500

    Improving the app_theme with a professional theme with Status Bar management.

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,38 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
     brightness: Brightness.light,
-    primaryColor: Colors.white,
     scaffoldBackgroundColor: Colors.white,
+    primaryColor: Colors.black,
     appBarTheme: const AppBarTheme(
       backgroundColor: Colors.white,
       elevation: 0,
+      systemOverlayStyle: SystemUiOverlayStyle.dark,
       iconTheme: IconThemeData(color: Colors.black),
-      titleTextStyle: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+      titleTextStyle: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
     ),
     bottomNavigationBarTheme: const BottomNavigationBarThemeData(
       backgroundColor: Colors.white,
       selectedItemColor: Colors.black,
       unselectedItemColor: Colors.grey,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      type: BottomNavigationBarType.fixed,
     ),
-    colorScheme: const ColorScheme.light(background: Colors.white, onBackground: Colors.black),
+    dividerColor: Colors.grey[200],
   );
 
   static final darkTheme = ThemeData(
     brightness: Brightness.dark,
-    primaryColor: Colors.black,
     scaffoldBackgroundColor: Colors.black,
+    primaryColor: Colors.white,
     appBarTheme: const AppBarTheme(
       backgroundColor: Colors.black,
       elevation: 0,
+      systemOverlayStyle: SystemUiOverlayStyle.light,
       iconTheme: IconThemeData(color: Colors.white),
+      titleTextStyle: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
     ),
     bottomNavigationBarTheme: const BottomNavigationBarThemeData(
       backgroundColor: Colors.black,
       selectedItemColor: Colors.white,
       unselectedItemColor: Colors.grey,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      type: BottomNavigationBarType.fixed,
     ),
-    colorScheme: const ColorScheme.dark(background: Colors.black, onBackground: Colors.white),
+    dividerColor: Colors.grey[800],
   );
 }
