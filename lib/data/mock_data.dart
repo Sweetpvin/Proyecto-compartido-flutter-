5059bd288c747d5a71399eecdf590282d9b27e1e
KingSadid <SadiSex07@gmail.com>
2026-02-28 17:48:01 -0500

    Creating a MockData class for random images. This will simulate real users.

---
+++ lib/data/mock_data.dart
@@ -0,0 +1,19 @@
+class MockData {
+  // URLs de imágenes aleatorias (Picsum & Pravatar)
+  static const List<String> profileImages = [
+    'https://i.pravatar.cc/150?img=1',
+    'https://i.pravatar.cc/150?img=2',
+    'https://i.pravatar.cc/150?img=3',
+    'https://i.pravatar.cc/150?img=4',
+    'https://i.pravatar.cc/150?img=5',
+  ];
+
+  static const List<String> postImages = [
+    'https://picsum.photos/id/1015/600/600',
+    'https://picsum.photos/id/1016/600/800', // Vertical
+    'https://picsum.photos/id/1018/600/600',
+    'https://picsum.photos/id/1025/600/600',
+  ];
+
+  static const List<String> usernames = ['ana_dev', 'flutter_master', 'travel_mike', 'design_pro', 'lucia_art'];
+}
