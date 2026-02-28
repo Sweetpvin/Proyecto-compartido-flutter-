4539215d10152360a71cb0ee795a700be509f115
KingSadid <SadiSex07@gmail.com>
2026-02-28 18:09:27 -0500

    Refactoring the MockData class to only contain images using native Flutter components

--- lib/data/mock_data.dart
+++ lib/data/mock_data.dart
@@ -1,19 +1,19 @@
 class MockData {
-  // URLs de imágenes aleatorias (Picsum & Pravatar)
   static const List<String> profileImages = [
-    'https://i.pravatar.cc/150?img=1',
-    'https://i.pravatar.cc/150?img=2',
-    'https://i.pravatar.cc/150?img=3',
-    'https://i.pravatar.cc/150?img=4',
-    'https://i.pravatar.cc/150?img=5',
+    'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&q=80',
+    'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&q=80',
+    'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400&q=80',
+    'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=400&q=80',
+    'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400&q=80',
   ];
 
   static const List<String> postImages = [
-    'https://picsum.photos/id/1015/600/600',
-    'https://picsum.photos/id/1016/600/800', // Vertical
-    'https://picsum.photos/id/1018/600/600',
-    'https://picsum.photos/id/1025/600/600',
+    'https://images.unsplash.com/photo-1501854140884-074cf2b2c75d?w=800&q=80',
+    'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=800&q=80',
+    'https://images.unsplash.com/photo-1469474968028-56623f02e42e?w=800&q=80',
+    'https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=800&q=80',
+    'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?w=800&q=80',
   ];
 
-  static const List<String> usernames = ['ana_dev', 'flutter_master', 'travel_mike', 'design_pro', 'lucia_art'];
+  static const List<String> usernames = ['diana_dev', 'mark_codes', 'travel_loop', 'photo_daily', 'jess_ui'];
 }
